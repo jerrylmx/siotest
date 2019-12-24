1ded7f366cac0788438c48bad7eedd699c1c563a
jerrylmx <47772503+jerrylmx@users.noreply.github.com>
2019-12-23 17:18:23 -0800

    Update app.js

--- src/public/app.js
+++ src/public/app.js
@@ -1,8 +1,12 @@
 const socket = io();
 document.getElementById("content").innerText = new Date().getTime();
 let last = 0;
+let counter = 0;
 
 socket.on('sync', (data) => {
-    document.getElementById("content").innerText = new Date().getTime() - last;
+    if (counter % 10 === 0) {
+        document.getElementById("content").innerText = new Date().getTime() - last;
+    }
     last =  new Date().getTime();
+    counter++;
 });
